4f9b2afccf29cb3df3e0571aeeb3e0b3fec02296
25S19 <htet@chuo-ai.jp>
2026-01-08 03:45:36 +0000

    add css js

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const launchBtn = document.getElementById("launch-btn");
 const pathInput = document.getElementById("program-path");
     launchBtn.addEventListener("click", () => {
 const url = pathInput.value.trim();
+const file = fileInput.files[0];
+const reader = new FileReader();
+
+reader.onload = function(e) {
+    previewImage.src = e.target.result; // This "sets" the image source
+}
+reader.readAsDataURL(file);
 if (!url) {
 alert("index.html の場所を入力してください.");
 return;
